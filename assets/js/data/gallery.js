a051bd1c668cc793a27d89bb01db8950737f9d94
ecanaveral <ecanaveral@crescendocollective.com>
2022-12-01 15:37:14 +0800

    change structure

---
+++ assets/js/data/gallery.js
@@ -0,0 +1,44 @@
+const galleryData = [
+    {
+        img: "assets/img/mountain1.jfif",
+        title: "Mountain 1",
+        detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        img: "assets/img/mountain2.jpg",
+        title: "Mountain 2",
+        detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        img: "assets/img/mountain3.jpg",
+        title: "Mountain 3",
+        detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        img: "assets/img/ocean1.jpg",
+        title: "Ocean 1",
+        detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        img: "assets/img/ocean2.webp",
+        title: "Ocean 2",
+        detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        img: "assets/img/ocean3.jpg",
+        title: "Ocean 3",
+        detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        img: "assets/img/download.jpg",
+        title: "Galaxy 1",
+        detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        img: "assets/img/galaxy2.jpg",
+        title: "Galaxy 2",
+        detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+];
+
+export default galleryData;
