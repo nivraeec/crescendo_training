e7d916900395c460a1472073052a350a4075184d
Jeff Cartago <jcartago@crescendocollective.com>
2023-01-27 01:01:34 +0800

    text validation mkII

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -3,19 +3,50 @@ const func = {
         console.log("test");
         
         const fname = document.querySelector('[name="firstname"]');
+        const lname = document.querySelector('[name="lastname"]');
+        const mname = document.querySelector('[name="middlename"]');
+        const address = document.querySelector('[name="address"]');
         const form = document.querySelector('form');
+ 
         form.onsubmit = (e) => {
             e.preventDefault();
             const fnameIndex = fname.value.split(' ');
+            const mnameIndex = mname.value.split(' ');
+            const lnameIndex = lname.value.split(' ');
+            const addressIndex = address.value.split(' ');
            if(fnameIndex[0] === '') {
                 this.errorMessage(fname.parentNode, "Please enter your first name");
            }
-           console.log(fnameIndex);
+           if(mnameIndex[0] === '') {
+                this.errorMessage(mname.parentNode, "Please enter your middle name");
+           }
+           if(lnameIndex[0] === '') {
+                this.errorMessage(lname.parentNode, "Please enter your last name");
+           }
+           if(addressIndex[0] === '') {
+                this.errorMessage(address.parentNode, "Please enter your address");
+           }
+
+           console.log(address);
+           
         }
         fname.onkeyup = () => {
            if(!fname.value.length) this.errorMessage(fname.parentNode, "Please enter your first name");
            else this.removeErrorMessage(fname.parentNode);
         }
+        mname.onkeyup = () => {
+            if(!mname.value.length) this.errorMessage(mname.parentNode, "Please enter your middle name");
+            else this.removeErrorMessage(mname.parentNode);
+         }
+        lname.onkeyup = () => {
+            if(!lname.value.length) this.errorMessage(lname.parentNode, "Please enter your last name");
+            else this.removeErrorMessage(lname.parentNode);
+        }
+        address.onkeyup = () => {
+            if(!address.value.length) this.errorMessage(address.parentNode, "Please enter your address");
+            else this.removeErrorMessage(address.parentNode);
+        }
+        
     },
     errorMessage(container, errorText) {
         if(!container.classList.contains("form__input--invalid")){
