069a1a0e3e09e1cb44dc39fb6923fb149e7f113f
Jeff Cartago <jcartago@crescendocollective.com>
2023-01-27 00:27:03 +0800

    text validation

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -7,17 +7,28 @@ const func = {
         form.onsubmit = (e) => {
             e.preventDefault();
             const fnameIndex = fname.value.split(' ');
-           if(fnameIndex[0] == '') {
-            alert();
+           if(fnameIndex[0] === '') {
+                this.errorMessage(fname.parentNode, "Please enter your first name");
            }
-
            console.log(fnameIndex);
         }
-
-
+        fname.onkeyup = () => {
+           if(!fname.value.length) this.errorMessage(fname.parentNode, "Please enter your first name");
+           else this.removeErrorMessage(fname.parentNode);
+        }
     },
-    test: "earvin"
-
+    errorMessage(container, errorText) {
+        if(!container.classList.contains("form__input--invalid")){
+         container.classList.add("form__input--invalid");
+         container.insertAdjacentHTML('beforeend', `<div class="form__input--errorText">${errorText}</div>`);
+        } 
+    },
+    removeErrorMessage(container) {
+        if(container.classList.contains("form__input--invalid")){
+         container.classList.remove("form__input--invalid");  
+         container.querySelector(".form__input--errorText").remove();
+        }   
+    }
 };
 
 
