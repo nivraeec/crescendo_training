ab594c26d9f582927362d15cdae69d7536b1871e
Jeff Cartago <jcartago@crescendocollective.com>
2023-01-30 21:08:50 +0800

    select

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -1,11 +1,12 @@
 const func = {
     init() {
-        console.log("test");
+        console.log(document.querySelectorAll("input"));
         
         const fname = document.querySelector('[name="firstname"]');
         const lname = document.querySelector('[name="lastname"]');
         const mname = document.querySelector('[name="middlename"]');
         const address = document.querySelector('[name="address"]');
+        const course = document.querySelector('[name="course"]');
         const form = document.querySelector('form');
  
         form.onsubmit = (e) => {
@@ -14,36 +15,46 @@ const func = {
             const mnameIndex = mname.value.split(' ');
             const lnameIndex = lname.value.split(' ');
             const addressIndex = address.value.split(' ');
+            
            if(fnameIndex[0] === '') {
-                this.errorMessage(fname.parentNode, "Please enter your first name");
+                this.errorMessage(fname.parentNode, "<div class='errorMessage'>Please enter your first name </div>");
            }
            if(mnameIndex[0] === '') {
-                this.errorMessage(mname.parentNode, "Please enter your middle name");
+                this.errorMessage(mname.parentNode, "<div class='errorMessage'>Please enter your middle name </div>");
            }
            if(lnameIndex[0] === '') {
-                this.errorMessage(lname.parentNode, "Please enter your last name");
+                this.errorMessage(lname.parentNode, "<div class='errorMessage'>Please enter your last name </div>");
            }
            if(addressIndex[0] === '') {
-                this.errorMessage(address.parentNode, "Please enter your address");
+                this.errorMessage(address.parentNode, "<div class='errorMessage'>Please enter your address </div>");
            }
 
-           console.log(address);
+           if(!course.value) this.errorMessage(course.parentNode, "<div class='errorMessage'>Please select the course </div>");
            
         }
+
+        course.onchange = () =>{
+        
+            if (course.value.length) this.removeErrorMessage(course.parentNode);
+            console.log(course.value);
+        }
+
+     
+
         fname.onkeyup = () => {
-           if(!fname.value.length) this.errorMessage(fname.parentNode, "Please enter your first name");
+           if(!fname.value.length) this.errorMessage(fname.parentNode, "<div class='errorMessage'> Please enter your first name </div>");
            else this.removeErrorMessage(fname.parentNode);
         }
         mname.onkeyup = () => {
-            if(!mname.value.length) this.errorMessage(mname.parentNode, "Please enter your middle name");
+            if(!mname.value.length) this.errorMessage(mname.parentNode, "<div class='errorMessage'> Please enter your middle name </div>");
             else this.removeErrorMessage(mname.parentNode);
          }
         lname.onkeyup = () => {
-            if(!lname.value.length) this.errorMessage(lname.parentNode, "Please enter your last name");
+            if(!lname.value.length) this.errorMessage(lname.parentNode, "<div class='errorMessage'> Please enter your last name </div>");
             else this.removeErrorMessage(lname.parentNode);
         }
         address.onkeyup = () => {
-            if(!address.value.length) this.errorMessage(address.parentNode, "Please enter your address");
+            if(!address.value.length) this.errorMessage(address.parentNode, "<div class='errorMessage'> Please enter your address </div>");
             else this.removeErrorMessage(address.parentNode);
         }
         
