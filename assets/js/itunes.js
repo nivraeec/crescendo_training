4d856602a1ff82d5150631b85b445a229bf6da51
Ailo Noscal <anoscal@crescendocollective.com>
2023-02-23 21:06:54 +0800

    refactored /assets/js/itunes.js

--- assets/js/itunes.js
+++ assets/js/itunes.js
@@ -10,19 +10,22 @@ const popupClose = document.querySelector(".albumPopupContainer .close");
 function init() {
     fetch('https://itunes.apple.com/us/rss/topalbums/limit=100/json').then(function (response) {
         return response.json();
-    }).then(function (data) {
-        const entries = data.feed.entry;
-        // console.log(entries);
-        var htmlCard;
-        for (var i=0; i < entries.length; i++) {
-            var j = i + 1;
+    }).then(function ({ feed }) {
+        const { entry } = feed;
+        console.log(entry);
+        let htmlCard;
+        entry.forEach(function(item, key){
+            let rank = key + 1;
             htmlCard = document.createElement("div");
             htmlCard.classList.add('albumCard');
-            htmlCard.innerHTML = '<a href="#"><span>#' + j + '</span><img src="' + entries[i]['im:image'][2].label + '" alt="' + entries[i]["im:name"].label + '"><p class="albumTitle">' + entries[i]["im:name"].label + '</p></a>';
+            htmlCard.innerHTML =    `<a href="#">
+                                        <span>#${rank}</span>
+                                        <img src="${item['im:image'][2].label}" alt="${item["im:name"].label}">
+                                        <p class="albumTitle">${item["im:name"].label}</p>
+                                    </a>`;
             cardsContainer.appendChild(htmlCard);
-            // insertAfter(cardsContainer, htmlCard);
-        }
-        bg.style.backgroundImage = "url('" + entries[0]['im:image'][2].label + "')";
+        });
+        bg.style.backgroundImage = `url('${entry[0]['im:image'][2].label}')`;
     }).catch(function (err) {
         console.warn('Something went wrong.', err);
     });
@@ -32,37 +35,37 @@ function init() {
         const album = document.querySelectorAll(".albumCard a");
         albumImg.forEach(function(item){
             item.addEventListener("mouseover", function(){
-                bg.style.backgroundImage = "url('" + item.src + "')";
+                bg.style.backgroundImage = `url("${this.src}")`;
             });
-        });
-        albumImg.forEach(function(item){
             item.addEventListener("click", function(){
                 popup.style.bottom = "0";
-                popupOnly.querySelector("img").src = item.src;
+                popupOnly.querySelector("img").src = this.src;
                 setTimeout(() => {
                     popup.style.backgroundColor = "#00000080";
                 }, 400);
             });
         });
-    }, 1000);
-
+    }, 1e3);
+    
+    const hidePopup = "-100%";
+    const transparent = "#00000000";
     popupClose.addEventListener("click", function(){
         setTimeout(() => {
-            popup.style.bottom = "-100%";
+            popup.style.bottom = hidePopup;
         }, 250);
-        popup.style.backgroundColor = "#00000000";
+        popup.style.backgroundColor = transparent;
     });
 
-    popup.addEventListener('click', e => {
-        if (e.target !== popupOnly) {
-            setTimeout(() => {
-                popup.style.bottom = "-100%";
-            }, 250);
-            popup.style.backgroundColor = "#00000000";
-        } else {
-            return;
-        }
+    popup.addEventListener('click', function() {
+        setTimeout(() => {
+            this.style.bottom = hidePopup;
+        }, 250);
+        this.style.backgroundColor = transparent;
     });
+
+    popupOnly.onclick = e => {
+        e.stopPropagation();
+    }
 }
 
 // <HTML APPEND FUNCTION
